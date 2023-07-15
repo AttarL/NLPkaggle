ca789c69b5c2f68bb6f04be386cad0fbfd19928d
AttarL <100249393+AttarL@users.noreply.github.com>
2023-07-15 16:24:46 +0800

    Update tokenization.py

--- tokenization.py
+++ tokenization.py
@@ -13,8 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License. 
 
-global false,null,true
-false=False
+global False,null,true
+False=False
 null=''
 true=True
 
